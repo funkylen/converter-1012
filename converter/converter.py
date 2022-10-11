537195ddb866bbef272397dd5d17d8ed1b650dba
funkylen <funkylen@gmail.com>
2022-10-11 12:38:58 +0300

    Сантиметры в метры и обратно

--- converter/converter.py
+++ converter/converter.py
@@ -1,10 +1,21 @@
 CM_IN_M = 100
 
 def convert():
-    # Сантиметры в метры
-    number_input = int(input('Введите значение для перевода: '))
-
-    # Формула? 
-    number_input = number_input / CM_IN_M
+    print("Типы конвертации: \n 1. Сантиметры в метры\n 2. Метры в сантиметры")
     
-    print(number_input)
+    convert_type = int(input('Выберите тип конвертации:'))
+
+    number = int(input('Введите значение для перевода: '))
+
+    if convert_type == 1:
+        converted_number = number / CM_IN_M
+        from_type = 'CM'
+        to_type = 'M'
+    elif convert_type == 2:
+        converted_number = number * CM_IN_M
+        from_type = 'M'
+        to_type = 'CM'
+    else:
+        raise 'Неизвестный тип конвертации'
+
+    print(f'{number} {from_type} = {converted_number} {to_type}')
